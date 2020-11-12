b135168b95a4985aed89aee8adb07b0bd46ac666
Kirk Chen <rwk0119@yahoo.com.tw>
2020-11-12 14:40:39 +0100

    chore(deps): Bump cdk version from 1.63.0 to 1.73.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const {AwsCdkTypeScriptApp} = require('projen')
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.63.0',
+  cdkVersion: '1.73.0',
   name: 'jenkins-as-code-example',
 
   /* AwsCdkTypeScriptAppOptions */
